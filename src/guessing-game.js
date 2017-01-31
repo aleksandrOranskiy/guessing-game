7bed95ed454be0729c753c9bb3b0aabd96a250f6
Aleks <aranich@ya.ru>
2017-01-31 14:37:48 +0300

    guessing-game/Feature: test-passed-100

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor(min = 0, max = 0) {}
 
     setRange(min, max) {
 
+    	this.min = min;
+    	this.max = max;
     }
 
     guess() {
 
+    	return Math.ceil(this.min + (this.max - this.min) / 2);
     }
 
     lower() {
 
+    	this.setRange(this.min, this.guess());
     }
 
     greater() {
 
+    	this.setRange(this.guess(), this.max);
     }
 }
 
